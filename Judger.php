84a778a7e2edfe20525bf6708f003bf096c1539f
David Diao <daviddiaoyf@gmail.com>
2019-08-05 13:22:25 +0800

    updated to support NOJ 0.3.1

--- Judger.php
+++ Judger.php
@@ -2,7 +2,7 @@
 namespace App\Babel\Extension\uvalive;
 
 use App\Babel\Submit\Curl;
-use App\Models\SubmissionModel;
+use App\Models\Submission\SubmissionModel;
 use App\Models\JudgerModel;
 use App\Models\OJModel;
 use Requests;
